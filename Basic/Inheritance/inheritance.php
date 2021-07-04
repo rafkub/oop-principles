2972353c1ee0445c1aa6dd997bfa92835a31b60e
rafkub <rafkub@example.com>
2021-07-04 16:30:07 +0100

    #3 feat, docs: Illustrate inheritance
    - Code without inheritance
    - Code with inheritance
    - README.md with short definition of inheritance
    
    Closes #3

---
+++ Basic/Inheritance/inheritance.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OOP\Principles\Basic\Inheritance;
+
+// a class which is inherited from is called base, super or parent class
+class Person // might be defined abstract if its instantiation is forbidden
+{
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function introduceYourself(): string
+    {
+        return "My name is $this->name.";
+    }
+}
+
+// a class which inherits from another one is called a subclass, derived class or a child class
+class Student extends Person // a student inherits person's features
+{
+    private int $grade;
+
+    public function __construct(string $name, int $grade)
+    {
+        parent::__construct($name);
+        $this->grade = $grade;
+    }
+
+    public function introduceYourself(): string
+    {
+        return parent::introduceYourself() . " I am in $this->grade grade."; // code reuse is possible
+    }
+}
+
+class Teacher extends Person  // a teacher also inherits person's features
+{
+    private string $subject;
+
+    public function __construct(string $name, string $subject)
+    {
+        parent::__construct($name);
+        $this->subject = $subject;
+    }
+
+    public function introduceYourself(): string
+    {
+        return parent::introduceYourself() . " I teach $this->subject.";
+    }
+}
+
+// Usage is identical as in without-inheritance.php:
+echo 'Student:' . PHP_EOL;
+$student = new Student('Tom', 5);
+echo $student->introduceYourself() . PHP_EOL;
+
+echo 'Teacher:' . PHP_EOL;
+$teacher = new Teacher('Eva', 'science');
+echo $teacher->introduceYourself() . PHP_EOL;
